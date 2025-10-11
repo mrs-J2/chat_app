d6716658775155f281a0b7a1e2d413e3b32b9599
lamis <lamisdhaouadi25@gmail.com>
2025-10-11 16:37:18 +0100

    Initial commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,88 @@
+import 'package:chat_app/components/my_button.dart';
+import 'package:chat_app/components/my_textfield.dart';
+import 'package:flutter/material.dart';
+class LoginPage extends StatelessWidget{
+  //email and pwdtxt controllers
+final TextEditingController _emailController = TextEditingController();
+final TextEditingController _pwController = TextEditingController();
+
+
+  LoginPage({super.key});
+
+  //login methode
+  void login(){
+    //login impl
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //logo
+            Icon(Icons.message,
+            size: 60,
+            color: Theme.of(context).colorScheme.primary,
+            ),
+            const SizedBox(height: 50),
+          
+            //welcome back message
+            Text(
+              "Welcome back dude",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 25),
+        
+            //email textfield
+            MyTextfield(
+              hintText: "Email" ,
+              obscureText: false,
+              controller: _emailController,
+            ),  
+            const SizedBox(height: 10),
+            //password textfield
+            MyTextfield(
+              hintText: "Password",
+              obscureText: true,
+              controller: _pwController),
+
+              const SizedBox(height: 25),
+            //login button
+            MyButton(
+              text: "Login",
+              onTap: login,
+            ),
+            const SizedBox(height: 25),
+
+            //register now
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "new user? ",
+                  style: 
+                  TextStyle(color: Theme.of(context).colorScheme.primary),
+                  ),
+
+                Text(
+                  "register now ",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.primary
+                  ),
+                  )
+              ],
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
+}
